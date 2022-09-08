b079c7819116ad7c84831b5facf5d517393a7f6b
jesusenerio <jesusenerio50@gmail.com>
2022-09-08 12:26:36 -0700

    low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+#include <stdio.h>                                                                                                              
+
+                                                                                                                                
+
+int main() {                                                                                                                    
+
+                                                                                                                                
+
+        char str1[] = "programming is like building a multilingual puzzle";                                                     
+
+                                                                                                                                
+
+        puts(str1);                                                                                                             
+
+                                                                                                                                
+
+        return(0);                                                                                                              
+
+}  
---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+	char str1[] = "programming is like building a multilingual puzzle";
+
+	puts(str1);
+
+	return(0);
+}

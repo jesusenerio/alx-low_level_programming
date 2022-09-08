e904c8cd2449ed6e487bcd2246bde04b3b082031
jesusenerio <jesusenerio50@gmail.com>
2022-09-08 15:22:14 -0700

    low level programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is main
+ * printf: this print on screen
+ * Return: this return value on screen
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

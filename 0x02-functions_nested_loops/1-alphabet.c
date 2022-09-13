6205f1eb6f4bba00d718bad07321b8b3fe932d8d
jesusenerio <jesusenerio50@gmail.com>
2022-09-13 12:03:42 -0700

    low level

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - this is main
+ * print_alphabet - this is a fuction that prints
  * putchar: prints
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+}
+/**
+ * main - this is main
  * Return: 0
  */
 int main(void)
 {
-	int print_alphabet(void)
-	{
-		int ch;
-
-		for (ch = 'a'; ch <= 'z'; ch++)
-		{
-			putchar(ch);
-			putchar('\n');
-		}
-	}
 	print_alphabet();
 	return (0);
 }

0d0d21f75026f24fdb0e7979335e6e2afe6e149e
jesusenerio <jesusenerio50@gmail.com>
2022-09-16 10:03:40 -0700

    low

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_line - prints the function
+ * i: int
+ * @n: int new
+ * _putchar: prints
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (i <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i = n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}

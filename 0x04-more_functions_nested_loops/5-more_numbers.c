fb929137b0db7d8a7525d09f78d1ad987bd6fbe3
jesusenerio <jesusenerio50@gmail.com>
2022-09-16 08:49:45 -0700

    l

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,21 +9,18 @@
 
 void more_numbers(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	j = 1;
-	while (j <= 10)
+	for (i = 1; i <= 10; i++)
 	{
-		for (i = 0; i < 15; i++)
+		for (j = 0; j < 15; j++)
 		{
-			if (i >= 10)
+			if (j >= 10)
 			{
 				_putchar('1');
-				_putchar(i % 10 + '0');
+				_putchar(j % 10 + '0');
 			}
-		_putchar('\n');
-		j++;
+			_putchar('\n');
 		}
 	}
 }

1583d73becac85017b8d063319b6134aca805fb1
jesusenerio <jesusenerio50@gmail.com>
2022-09-20 06:35:15 -0700

    p

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - length to 98;
+ * @s: points to s
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+		return (0);
+	}
+}

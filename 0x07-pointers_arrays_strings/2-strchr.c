e5786c57aca4821b15047957e93e55031fbff852
jesusenerio <jesusenerio50@gmail.com>
2022-09-27 13:31:55 -0700

    l

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strchr - delete strig before the word c then print
@@ -19,4 +18,5 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 	}
+	return (0);
 }

dfa90a86c15912bb816cc4664cb3b7494f12f9aa
jesusenerio <jesusenerio50@gmail.com>
2022-09-27 13:24:28 -0700

    l

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - delete strig before the word c then print
+ * @c: char to delete
+ * @s: string to delete from
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+}

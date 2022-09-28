7239c24cd954790270413f809142fd7b64c30d37
jesusenerio <jesusenerio50@gmail.com>
2022-09-27 20:23:12 -0700

    l

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,17 +10,16 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0, r = 0;
+	int i;
 
+	unsigned int sum, sum1;
+
+	sum = 0;
+	sum1 = 0;
 	for (i = 0; i < size; i++)
 	{
-		p = (i * size) + i;
-		l += *(a + p);
+		sum += a[(size * i) + i];
+		sum += a[(size * (i + 1)) - (i + 1)];
 	}
-	for (j = 0; j < size; j++)
-	{
-		p = (j * size) + (size - l - j);
-		r += *(a + p);
-	}
-	printf("%i, %i\n", l, r);
+	printf("%d, %d\n", sum, sum1);
 }

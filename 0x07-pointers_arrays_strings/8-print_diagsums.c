292858be2175d52ee4366745a768ca290897c0ad
jesusenerio <jesusenerio50@gmail.com>
2022-09-27 19:50:56 -0700

    l

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagsums -  sum of diagonal
+* @a: pointers
+* @size: the size
+* Return: 0
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - l - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}

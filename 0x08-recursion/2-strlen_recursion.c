805bdea98082616ba2a13ff2de8635b759af26c7
jesusenerio <jesusenerio50@gmail.com>
2022-09-29 13:01:26 -0700

    l

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - this gets the length of a string
+ * @s: this gets the length of the string
+ * Return: s
+ */
+
+int _strlen_recursion(char *s)
+{
+	int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum ++;
+		sum += _strlen_recursion(s + 1);
+	}
+}

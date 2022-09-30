6b4d3fd43ad037fcb7c54fd35ac01cce50652269
jesusenerio <jesusenerio50@gmail.com>
2022-09-30 04:59:44 -0700

    l

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the argv
+ * @argc: this count the argument
+ * @argv: this print name of the argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

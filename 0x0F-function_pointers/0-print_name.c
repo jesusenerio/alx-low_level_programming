99f0ed160c78e8770fbff194fb0e938fbd133c48
jesusenerio <jesusenerio50@gmail.com>
2022-10-14 08:48:12 -0700

    func

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints the name on the screen
+ * @name: pointer to name
+ * @f: pointer to f
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (&name || &f)
+		return;
+	f(name);
+}

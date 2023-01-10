388ce31127140c1aeda8cd5d08ee51e2972119c0
jesusenerio <jesusenerio50@gmail.com>
2023-01-10 07:01:14 -0800

    c

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,4 @@
-#include "hast_tables.h"
+#include "hash_tables.h"
 
 /**
  * hash_djb2 - Hash function implementing the djb2 algorithm.
@@ -8,10 +8,12 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash = 5381;
+	unsigned long int hash;
 	int c;
 
-	while((c = *str++))
-		hash = ((hash << 5) + hash) + c;
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
 	return (hash);
 }
